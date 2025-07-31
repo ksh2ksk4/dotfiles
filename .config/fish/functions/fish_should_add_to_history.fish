9c4f136234a54d89384de07fc046a4f97db2e1b5
ksh2ksk4 <github.clicker425@passinbox.com>
2025-08-01 01:08:15 +0900

    [fish shell] Update file

--- .config/fish/functions/fish_should_add_to_history.fish
+++ .config/fish/functions/fish_should_add_to_history.fish
@@ -1,11 +1,11 @@
 function fish_should_add_to_history
     # 履歴に残したくないコマンド(引数なし)
-    set -l commands 'bg' 'cd' 'exit' 'fg' 'history' 'jobs' 'la' 'll' 'ls' 'pwd' 'top' 'tree'
+    set -l commands 'bg' 'cd' 'exit' 'fg' 'history' 'jobs' 'la' 'll' 'ls' 'pwd' 'set' 'top' 'tree'
 
     contains $argv $commands; and return 1
 
     # 履歴に残したくないコマンド(引数あり)
-    set -l commandsWithArgs 'which'
+    set -l commandsWithArgs 'cat' 'kill' 'which'
 
     for c in $commandsWithArgs
         string match -q -r "^$c" $argv; and return 1
