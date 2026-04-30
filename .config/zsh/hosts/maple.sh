ce9d6192ee099fd8a3f410bbcbc090d623a20127
ksh2ksk4 <github.clicker425@passinbox.com>
2026-05-01 01:14:15 +0900

    [Zsh] Update files

--- .config/zsh/hosts/maple.sh
+++ .config/zsh/hosts/maple.sh
@@ -22,3 +22,12 @@ autoload -Uz add-zsh-hook
 add-zsh-hook chpwd tmux_set_status_style
 # フック関数を実行するため
 cd "${PWD}"
+
+#
+# Rust
+#   公式インストーラが .zshenv に追加した設定をここに移動
+#   (.zshenv に設定するのは行儀が悪い)
+#   他の OS ではパッケージマネージャでインストールしており、
+#   このスクリプトが存在しない
+#
+. "${HOME}/.cargo/env"
